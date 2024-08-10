b82cd476c3785e391e32b24a28683fef2c43f6c8
vaibhavbura <vaibhavbura@gmail.com>
2024-08-11 00:52:32 +0530

    navbar&main-container

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.getElementById('file-upload').addEventListener('change', function(event) {
+    const preview = document.getElementById('preview-img');
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+    };
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none';
+    }
+});
